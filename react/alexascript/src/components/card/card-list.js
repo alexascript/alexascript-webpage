6c1366e9dab52b93ca38e5d3d3dcb38c5aa614d2
alexandra Zapata <alexa3141@gmail.com>
2024-03-11 10:27:48 -0500

    adds links to github and linkedin cards

--- react/alexascript/src/components/card/card-list.js
+++ react/alexascript/src/components/card/card-list.js
@@ -4,15 +4,21 @@ import { Card } from "./card";
 export function CardList() {
   return (
     <ul className="card-list">
-      <li>
-        <Card platform="GitHub" logoImg="assets/github-logo.png" />
-      </li>
-      <li>
-        <Card platform="LinkedIn" logoImg="assets/linkedin-logo.png" />
-      </li>
-      <li>
-        <Card platform="CV" logoImg="assets/cv-icon.png" />
-      </li>
+      <a href="https://github.com/alexascript" target="blank">
+        <li>
+          <Card platform="GitHub" logoImg="assets/github-logo.png" />
+        </li>
+      </a>
+      <a href="https://www.linkedin.com/in/alexascript/" target="blank">
+        <li>
+          <Card platform="LinkedIn" logoImg="assets/linkedin-logo.png" />
+        </li>
+      </a>
+      <a>
+        <li>
+          <Card platform="CV" logoImg="assets/cv-icon.png" />
+        </li>
+      </a>
     </ul>
   );
 }
